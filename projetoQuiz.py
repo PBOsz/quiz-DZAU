2873df7f6de4a65f8ef013b67636b86cd9afdf39
PBOsz <175992242+PBOsz@users.noreply.github.com>
2026-09-08 20:36:40 -0400

    Update projetoQuiz.py

--- projetoQuiz.py
+++ projetoQuiz.py
@@ -26,7 +26,7 @@ MIX = '5'
 opcao_user = input('Digite o número da opção: ')
 if opcao_user == MATEMÁTICA:
  print('Quiz de Matemática do DZAU iniciando' )
- print(Um número é uma representação abstrata da quantidade que queremos quantificar, enquanto o numeral é o desenho ou símbolo utilizado para representar essa quantidade.\n (B)TESTE.\n (C)teste.\n')
+ print('Pela definição, o que é um número e um numeral?\n (A) Um número é uma representação abstrata da quantidade que queremos quantificar, enquanto o numeral é o desenho ou símbolo utilizado para representar essa quantidade.\n (B)TESTE.\n (C)teste.\n')
  resp_1 = input('Resposta: ').strip().upper()
  
  if resp_1 == 'A':
